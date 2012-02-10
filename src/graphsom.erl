680a80136a7d2eb6f1132af8a832aaea0a3bbfac
Usman Ismail <0xfffffff@gmail.com>
2012-02-09 22:16:18 -0500

    Moving API methods to graphson.erl so that graphsom_timer is only for timer related methods

--- src/graphsom.erl
+++ src/graphsom.erl
@@ -1,3 +1,36 @@
 -module(graphsom).
 
 %% all graphsom api would go in here
+-export([send_to_graphite/3, collect_metrics/2]).
+ 
+%% TODO collect non-gauge metrics
+collect_metrics(MetricStr, [MetricName | T]) ->
+    MetricStr2 = case catch folsom_metrics:get_metric_value(MetricName) of
+                     {Error, Reason} ->
+                         error_logger:info_msg("Error when getting metric from folsom_metrics: error: ~p, reason: ~p",[Error, Reason]),
+                         MetricStr;
+                     MetricValue ->
+                         error_logger:info_msg("Got value ~p for metric ~p \n",[MetricValue, MetricName]),
+                         CurrentTime = current_time(),
+                         string:concat(MetricStr , io_lib:format("p ~p ~w ~n",
+                                                                 [MetricName
+                                                                  ,MetricValue, CurrentTime]))
+                 end,
+    collect_metrics(MetricStr2, T);
+
+collect_metrics(MetricStr, []) ->	
+	MetricStr.
+   
+send_to_graphite(MetricStr, GraphiteHost, GraphitePort) ->
+    case gen_tcp:connect(GraphiteHost, GraphitePort, [list, {packet, 0}]) of
+        {ok, Sock} ->
+            gen_tcp:send(Sock, MetricStr),
+            gen_tcp:close(Sock),
+            ok;
+        {error, Reason} ->
+            error_logger:error_msg("Failed to connect to graphite: ~p", [Reason]),
+            {error, Reason}
+    end. 
+
+current_time() ->    
+    calendar:datetime_to_gregorian_seconds(calendar:now_to_universal_time( now()))-719528*24*3600. 
--- src/graphsom_timer.erl
+++ src/graphsom_timer.erl
@@ -35,9 +35,9 @@ init([ReportIntervalMs, GraphiteHost, GraphitePort]) ->
 handle_cast({report}, State) -> 
     Metrics = folsom_metrics:get_metrics(),
     io:format("List of metrics: ~w", [Metrics]),
-    MetricStr = collect_metrics([], Metrics),
+    MetricStr = graphsom:collect_metrics([], Metrics),
     io:format("Metric string: ~p", [MetricStr]),
-    send_to_graphite(lists:flatten(MetricStr),
+    graphsom:send_to_graphite(lists:flatten(MetricStr),
                      State#state.graphite_host, State#state.graphite_port),
     {noreply, State};
 	
@@ -63,34 +63,4 @@ handle_info(_Info, State) ->
 stop() ->
     gen_server:cast(?MODULE, stop).	
 
-%% TODO collect non-gauge metrics
-collect_metrics(MetricStr, [MetricName | T]) ->
-    MetricStr2 = case catch folsom_metrics:get_metric_value(MetricName) of
-                     {Error, Reason} ->
-                         error_logger:info_msg("Error when getting metric from folsom_metrics: error: ~p, reason: ~p",[Error, Reason]),
-                         MetricStr;
-                     MetricValue ->
-                         error_logger:info_msg("Got value ~p for metric ~p \n",[MetricValue, MetricName]),
-                         CurrentTime = current_time(),
-                         string:concat(MetricStr , io_lib:format("p ~p ~w ~n",
-                                                                 [MetricName
-                                                                  ,MetricValue, CurrentTime]))
-                 end,
-    collect_metrics(MetricStr2, T);
-
-collect_metrics(MetricStr, []) ->	
-	MetricStr.
    
-send_to_graphite(MetricStr, GraphiteHost, GraphitePort) ->
-    case gen_tcp:connect(GraphiteHost, GraphitePort, [list, {packet, 0}]) of
-        {ok, Sock} ->
-            gen_tcp:send(Sock, MetricStr),
-            gen_tcp:close(Sock),
-            ok;
-        {error, Reason} ->
-            error_logger:error_msg("Failed to connect to graphite: ~p", [Reason]),
-            {error, Reason}
-    end. 
-
-current_time() ->    
-    calendar:datetime_to_gregorian_seconds(calendar:now_to_universal_time( now()))-719528*24*3600.    

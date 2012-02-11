fb4374ea2e779637ae3d062c28bcaee53743dff9
Bilal Sheikh <bilalsheikh298@gmail.com>
2012-02-11 15:24:07 -0500

    Add specs to graphsom_app

--- include/graphsom.hrl
+++ include/graphsom.hrl
@@ -1,3 +1,8 @@
+-type start_type() :: normal
+                    | {takeover, Node :: node()}
+                    | {failover, Node :: node()}.
+
+
 -type system_stats_type() :: list().
 
 
--- src/graphsom_app.erl
+++ src/graphsom_app.erl
@@ -5,15 +5,25 @@
 %% Application callbacks
 -export([start/0, start/2, stop/1]).
 
+%% Includes
+-include("graphsom.hrl").
+
 %% ===================================================================
 %% Application callbacks
 %% ===================================================================
 
+
+-spec start(start_type(), term()) -> {ok, pid()} | {error, term()}.
+
 start(_StartType, _StartArgs) ->
     start().
+
+-spec start() -> {ok, pid()} | {error, term()}.
 
 start() ->
     graphsom_sup:start_link().
 
-stop(_State) ->
+-spec stop([]) -> ok. 
+
+stop([]) ->
     ok.

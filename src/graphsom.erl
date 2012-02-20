eeb672fcebd103986cc78bdf4641c2a0237bb3ea
Bilal Sheikh <bilalsheikh298@gmail.com>
2012-02-20 03:37:11 -0500

    cleanup graphsom:report

--- src/graphsom.erl
+++ src/graphsom.erl
@@ -11,19 +11,20 @@
 
 -spec report_metrics(string(), pos_integer(), string(), list(), boolean()) -> ok | {error, term()}.
 
-report_metrics(GraphiteHost, GraphitePort, GraphitePrefix, VmMetrics, ReportAllMetrics) ->
-    %% check whether to report all user created metrics or not
-    Metrics = case ReportAllMetrics of
-                  true ->
-                      folsom_metrics:get_metrics();
-                  _ ->
-                      []
-              end,
-    io:format("List of metrics: ~w ~n", [Metrics]),
-    io:format("List of vm metrics ~w, ~n", [VmMetrics]),
-    CurrentTime = graphsom_util:current_time(),
-    VmMetricStr= graphsom_folsom:stringify_vm_metrics(VmMetrics, GraphitePrefix, CurrentTime),
-    UserMetricStr = graphsom_folsom:stringify_metrics(Metrics, GraphitePrefix, CurrentTime),
+report_metrics(GraphiteHost, GraphitePort, GraphitePrefix, VmMetrics, true) ->
+    report_metrics(folsom_metrics:get_metrics(), VmMetrics, GraphiteHost, GraphitePort, GraphitePrefix, graphsom_util:current_time());
+
+report_metrics(GraphiteHost, GraphitePort, GraphitePrefix, VmMetrics, false) ->    
+    report_metrics([], VmMetrics, GraphiteHost, GraphitePort, GraphitePrefix, graphsom_util:current_time()).
+
+%% Internal API
+
+-spec report_metrics(list(), list(), string(), pos_integer(), string(), pos_integer()) -> ok | {error | term()}.
+
+report_metrics(Metrics, VmMetrics, GraphiteHost, GraphitePort, GraphitePrefix, CurTime) ->
+    VmMetricStr= graphsom_folsom:stringify_vm_metrics(VmMetrics, GraphitePrefix, CurTime),
+    UserMetricStr = graphsom_folsom:stringify_metrics(Metrics, GraphitePrefix, CurTime),
     MetricStr = lists:flatten([VmMetricStr, UserMetricStr]),
     io:format("Metric string: ~s ~n", [MetricStr]),
     graphsom_graphite:report(MetricStr, GraphiteHost, GraphitePort).
+

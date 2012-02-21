63f381e8561df6cd50504d4c7db05887edf89f30
Bilal Sheikh <bilalsheikh298@gmail.com>
2012-02-20 21:28:55 -0500

    ability to register folsom metrics to be reported

--- src/graphsom.erl
+++ src/graphsom.erl
@@ -1,7 +1,7 @@
 -module(graphsom).
 
 %% all graphsom api would go in here
--export([report_metrics/5]).
+-export([register_folsom_metric/1]).
 
 %% Includes
 
@@ -9,25 +9,13 @@
 
 %% API
 
--spec report_metrics(string(), pos_integer(), string(), list(), boolean()) -> ok | {error, term()}.
+-spec register_folsom_metric(folsom_metric_name_type()) -> ok | {error, term()}.
 
-report_metrics(GHost, GPort, GPrefix, VmMetrics, true) ->
-    report_metrics(folsom_metrics:get_metrics(), VmMetrics, GHost, GPort, GPrefix, graphsom_util:current_time());
-
-report_metrics(GHost, GPort, GPrefix, VmMetrics, false) ->    
-    report_metrics([], VmMetrics, GHost, GPort, GPrefix, graphsom_util:current_time()).
-
-%% Internal API
-
--spec report_metrics(list(), list(), string(), pos_integer(), string(), pos_integer()) -> ok | {error | term()}.
-
-report_metrics(Metrics, VmMetrics, GHost, GPort, GPrefix, CurTime) ->
-    MetricStr = stringify_metrics(Metrics, VmMetrics, GPrefix, CurTime),
-    io:format("Metric string: ~s ~n", [MetricStr]),
-    graphsom_graphite:report(MetricStr, GHost, GPort).
-
--spec stringify_metrics(list(), list(),  string(), pos_integer()) -> string().
-
-stringify_metrics(Metrics, VmMetrics, GPrefix, CurTime) ->
-    MList = graphsom_folsom:get_metrics(Metrics, VmMetrics),
-    lists:flatten([graphsom_graphite:stringify_proplist_metric(Name, Val, GPrefix, CurTime, "")|| {Name, Val} <- MList]).
+register_folsom_metric(FolsomMetric) ->
+    case whereis(graphsom_timer) of
+        undefined ->
+            {error, graphsom_timer_not_found};
+        Gt ->
+            gen_server:cast(Gt, {register, FolsomMetric}),
+            ok
+    end.
--- src/graphsom_timer.erl
+++ src/graphsom_timer.erl
@@ -8,27 +8,28 @@
 
 -include("graphsom.hrl").
 
--record(state, {report_interval,            % ms interval between stat reporting
-                report_timer, 	            % TRef of interval timer
-                graphite_host,              % graphite server host
-                graphite_port,	            % graphite server port
-                graphite_prefix,            % prefix added to all graphite keys
-                vm_metrics,	            % list of vm metrics to report
-                report_all_user_metrics     % whether to report all user defined metrics
+-record(state, {report_interval,              % ms interval between stat reporting
+                report_timer, 	              % TRef of interval timer
+                graphite_host,                % graphite server host
+                graphite_port,	              % graphite server port
+                graphite_prefix,              % prefix added to all graphite keys
+                vm_metrics,	              % list of vm metrics to report
+                report_all_folsom_metrics,    % whether to report all folsom  metrics
+                folsom_metrics                % list of registered folsom metrics
                }).               
                
 -type state() :: #state{}.
 
 -spec start_link(pos_integer(), string(), pos_integer(), string(), list(), boolean()) -> {ok, pid()} | {error, term()}.
 
-start_link(ReportIntervalMs, GraphiteHost, GraphitePort, Prefix, VmMetrics, AllUserMetrics) ->
+start_link(ReportIntervalMs, GraphiteHost, GraphitePort, Prefix, VmMetrics, AllFolsomMetrics) ->
     %% io:format("graphsom_timer started ~n"),
     gen_server:start_link({local, ?MODULE}, ?MODULE,  
-                          [ReportIntervalMs, GraphiteHost, GraphitePort, Prefix, VmMetrics, AllUserMetrics], []).
+                          [ReportIntervalMs, GraphiteHost, GraphitePort, Prefix, VmMetrics, AllFolsomMetrics], []).
 
 -spec init(list()) -> {ok, state()}.
 
-init([ReportIntervalMs, GraphiteHost, GraphitePort, Prefix, VmMetrics, AllUserMetrics]) ->
+init([ReportIntervalMs, GraphiteHost, GraphitePort, Prefix, VmMetrics, AllFolsomMetrics]) ->
     %% io:format("graphsom will report stats to ~p:~p every ~p ms ~n",
     %%          [ GraphiteHost, GraphitePort, ReportIntervalMs ]),
     {ok, Tref} = timer:apply_interval(ReportIntervalMs, gen_server, cast,  [?MODULE, report]),                     
@@ -39,7 +40,8 @@ init([ReportIntervalMs, GraphiteHost, GraphitePort, Prefix, VmMetrics, AllUserMe
       graphite_port = GraphitePort,
       graphite_prefix = Prefix,
       vm_metrics = VmMetrics,
-      report_all_user_metrics = AllUserMetrics
+      report_all_folsom_metrics = AllFolsomMetrics,
+      folsom_metrics = []
      },
     %% io:format("graphsom_timer Vm Metrics: ~w ~n", [VmMetrics]),
     {ok, State}.
@@ -48,11 +50,20 @@ init([ReportIntervalMs, GraphiteHost, GraphitePort, Prefix, VmMetrics, AllUserMe
 
 -spec handle_cast(cast_msg_type(), state()) -> {noreply, state()} | {stop, normal, state()}.
 
-handle_cast(report, State) -> 
-    _ = graphsom:report_metrics(State#state.graphite_host, State#state.graphite_port, 
-                            State#state.graphite_prefix, State#state.vm_metrics,
-                            State#state.report_all_user_metrics),
+handle_cast(report, State = #state{ graphite_host = GHost, graphite_port = GPort, graphite_prefix = GPrefix}) ->
+    VmMetrics = State#state.vm_metrics,
+    Metrics = case State#state.report_all_folsom_metrics of
+                  true ->
+                      folsom_metrics:get_metrics();
+                  _ ->
+                      State#state.folsom_metrics
+              end,
+    _ = report_metrics(Metrics, VmMetrics, GHost, GPort, GPrefix),
     {noreply, State};
+
+handle_cast({register, FolsomMetric}, State) ->
+    State1 = State#state{ folsom_metrics = folsom_metrics ++ [FolsomMetric]},
+    {noreply, State1};
 	
 handle_cast(stop, State) ->
    {stop, normal, State};
@@ -85,4 +96,20 @@ handle_info(_Info, State) ->
 stop() ->
     gen_server:cast(?MODULE, stop).	
 
-   
+%% Internal API
+
+-spec report_metrics(list(), list(), string(), pos_integer(), string()) -> ok | {error | term()}.
+
+report_metrics(Metrics, VmMetrics, GHost, GPort, GPrefix) ->
+    CurTime = graphsom_util:current_time(),
+    MetricStr = stringify_metrics(Metrics, VmMetrics, GPrefix, CurTime),
+    io:format("Metric string: ~s ~n", [MetricStr]),
+    graphsom_graphite:report(MetricStr, GHost, GPort).
+
+-spec stringify_metrics(list(), list(),  string(), pos_integer()) -> string().
+
+stringify_metrics(Metrics, VmMetrics, GPrefix, CurTime) ->
+    MList = graphsom_folsom:get_metrics(Metrics, VmMetrics),
+    lists:flatten([graphsom_graphite:stringify_proplist_metric(Name, Val, GPrefix, CurTime, "")|| {Name, Val} <- MList]).
+
+

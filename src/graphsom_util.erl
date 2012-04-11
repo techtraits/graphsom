8f826c9da8f9fe9dd0105b6c884977b1d0dc3a4b
Bilal Sheikh <bilalsheikh298@gmail.com>
2012-04-11 02:09:19 -0400

    add utility function to check for list of numbers

--- src/graphsom_util.erl
+++ src/graphsom_util.erl
@@ -1,6 +1,8 @@
 -module(graphsom_util).
 
--export([current_time/0, mean/1]).
+-include_lib("eunit/include/eunit.hrl").
+
+-export([current_time/0, mean/1, is_num_list/1]).
 
 -spec mean(list()) -> number().
 
@@ -16,4 +18,26 @@ mean([], C, S) -> S/C.
 
 current_time() ->    
     calendar:datetime_to_gregorian_seconds(calendar:now_to_universal_time( now()))-719528*24*3600. 
+
+-spec is_num_list(list()) -> boolean().
+
+is_num_list([]) -> true;
+is_num_list([H|T]) when is_number(H) -> is_num_list(T);
+is_num_list(_) -> false.
+
+%% UNIT TESTS %%
+
+-spec is_num_list_test() -> ok.
+
+is_num_list_test() ->
+    ?assertEqual(true, is_num_list([])),
+    ?assertEqual(true, is_num_list([123.2])),
+    ?assertEqual(true, is_num_list([1, 2, 3, 5 ,9])),
+    ?assertEqual(true, is_num_list([12, 0.23])),
+    ?assertEqual(false, is_num_list([23, [767], 90])),
+    ?assertEqual(false, is_num_list([120, {count, 123}])),
+    ?assertEqual(false, is_num_list(405)),
+    ?assertEqual(false, is_num_list({23, 65})).
+
+-spec test() -> ok.
 

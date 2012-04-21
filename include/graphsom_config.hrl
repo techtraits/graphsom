8a9f15dda4eae03d433375ab6496e143413e0fc1
Bilal Sheikh <bilalsheikh298@gmail.com>
2012-04-21 00:54:39 -0400

    default graphsom backend configurations

---
+++ include/graphsom_config.hrl
@@ -0,0 +1,17 @@
+%% Default configurations for various visualization adaptors
+%% Currently Graphite and Collectd
+
+-define(GRAPHSOM_BACKENDS, [graphite, collectd]).
+
+-define(GRAPHSOM_GRAPHITE, 
+            [
+             {report_interval, 30000}, %% report interval (ms)
+             {host, "localhost"},
+             {port, 2003},
+             {prefix, "graphsom"}, %% prefix for all metrics
+             {vm_metrics, []},
+             {report_all, false} %% report all folsom metrics
+            ]
+        ).
+
+-define(GRAPHSOM_COLLECTD, []).
